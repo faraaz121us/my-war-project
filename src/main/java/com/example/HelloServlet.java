de509d420271b57cd80f1a492e1170cb4c9c25e3
faraz <faraaz@gmail.com>
2023-09-22 13:43:36 +0000

    1st commit V1

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body><h2>Hi, it's Sarfaraz</h2></body></html>");
+    }
+}
+
